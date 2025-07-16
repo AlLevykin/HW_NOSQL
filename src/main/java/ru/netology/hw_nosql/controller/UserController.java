ebdd5111928495b52c652f5a9a7aad57bd775a67
Левыкин Алексей Александрович <alevykin@alfabank.ru>
2025-07-17 00:05:10 +0500

    @RequiredArgsConstructor applied

--- src/main/java/ru/netology/hw_nosql/controller/UserController.java
+++ src/main/java/ru/netology/hw_nosql/controller/UserController.java
@@ -7,31 +7,28 @@ import ru.netology.hw_nosql.service.UserService;
 
 import java.util.List;
 
-@RequestMapping("/")
+@RequiredArgsConstructor
 @RestController
+@RequestMapping("/users")
 public class UserController {
     private final UserService userService;
 
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
-    @GetMapping("/users")
+    @GetMapping
     public List<User> getAllUsers() {
         return userService.getUsers();
     }
 
-    @GetMapping("/users/{id}")
+    @GetMapping("/{id}")
     public User getUserById(@PathVariable String id) {
         return userService.getUser(id);
     }
 
-    @PostMapping("/users")
+    @PostMapping
     public User createUser(@RequestBody User user) {
         return userService.saveUser(user);
     }
 
-    @DeleteMapping("/users/{id}")
+    @DeleteMapping("/{id}")
     public void deleteUser(@PathVariable String id) {
         userService.deleteUser(id);
     }
--- src/main/java/ru/netology/hw_nosql/service/UserService.java
+++ src/main/java/ru/netology/hw_nosql/service/UserService.java
@@ -7,13 +7,10 @@ import ru.netology.hw_nosql.repository.UserRepository;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @Service
 public class UserService {
     private final UserRepository userRepository;
-
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
 
     public List<User> getUsers() {
         return userRepository.findAll();
