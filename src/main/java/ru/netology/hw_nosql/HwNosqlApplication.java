ee3cd783a3706c9774f2bb787bd220896ec594ad
Levykin <a.a.levykin@gmail.com>
2025-07-16 08:05:18 +0500

    init

---
+++ src/main/java/ru/netology/hw_nosql/HwNosqlApplication.java
@@ -0,0 +1,13 @@
+package ru.netology.hw_nosql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwNosqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwNosqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/netology/hw_nosql/HwNosqlApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.hw_nosql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwNosqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
